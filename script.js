48620378dc404a80b3b3502fef5e8ac964bfb136
DangLun <danglun337747@gmail.com>
2024-09-28 11:43:25 +0700

    second commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function openMenu() {
+  const nav = document.querySelector("nav");
+  nav.classList.toggle("open");
+}
+
+function contactMe() {
+  location.href = "https://www.facebook.com/profile.php?id=100014323350786";
+}
